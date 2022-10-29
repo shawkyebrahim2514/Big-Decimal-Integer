288e560dbb8ff09aa1449b120e37a03f74fe6fbf
Shawky Ebrahim Ahmed <shawky.ebrahim2514@gmail.com>
2022-10-29 17:02:55 +0200

    update files

--- Project files/BigDecimalInt.cpp
+++ Project files/BigDecimalInt.cpp
@@ -1,42 +1,116 @@
 #include "BigDecimalInt.h"
-
 #include <iostream>
-BigDecimalInt::BigDecimalInt(int n)
-{
-    // append each digit from right to the class number deque
-    if(n < 0) sign = false;
-    else sign = true;
-    n = abs(n);
-    while (n)
+
+
+int BigDecimalInt::isValidNumber(const std::string &number) {
+    int start;
+    if(number[0] == '-' || number[0] == '+') start = 1;
+    else start = 0;
+    for (int i = start; i < number.size(); ++i)
     {
-        number.push_front(n%10);
-        n /= 10;
+        if(number[i] < '0' || number[i] > '9')
+        {
+            this->sign = true;
+            this->wholeNumber = {0};
+            return -1;
+        }
     }
+    return start;
 }
 
-BigDecimalInt::BigDecimalInt(std::string n)
-{
-    int start = isValidNumber(n);
+void BigDecimalInt::deleteZerosFromLeft() {
+    // delete leading zeros from the beginning of the wholeNumber (from right)
+    while (this->wholeNumber.size() > 1 && !this->wholeNumber.front())
+        this->wholeNumber.pop_front();
+    if(this->wholeNumber.size() == 1 && !this->wholeNumber[0]) this->sign = true;
+}
+
+void BigDecimalInt::setNumber(const std::string& number) {
+    int start = isValidNumber(number);
     if(!~start) return;
     // add the sign
     if(start)
     {
-        if(n[0] == '+') this->sign = 1;
-        else this->sign = 0;
+        if(number[0] == '+') this->sign = true;
+        else this->sign = false;
     }
-    else this->sign = 1;
-    // append each digit to the class number deque
-    for (int i = start; i < n.size(); ++i)
+    else this->sign = true;
+    // append each digit to the class wholeNumber deque
+    for (int i = start; i < number.size(); ++i)
     {
-        number.push_back(n[i]-'0');
+        this->wholeNumber.push_back(number[i]-'0');
     }
-    deleteZeros();
+    deleteZerosFromLeft();
+}
+
+BigDecimalInt::BigDecimalInt(const int& number)
+{
+    std::string numberString = std::to_string(number);
+    setNumber(numberString);
+}
+
+BigDecimalInt::BigDecimalInt(const std::string& number)
+{
+    setNumber(number);
+}
+
+BigDecimalInt::BigDecimalInt(const BigDecimalInt &anotherNumber) {
+    *this = anotherNumber;
+}
+
+BigDecimalInt::BigDecimalInt(BigDecimalInt &&anotherNumber) noexcept {
+    *this = anotherNumber;
+}
+
+void BigDecimalInt::doSummation(BigDecimalInt& a)
+{
+    // do summation between digits
+    int position = (int)this->wholeNumber.size() - 1;
+    while (position >= 0){
+        this->wholeNumber[position] += a.wholeNumber[position];
+        // if the wholeNumber is greater than 9
+        // we should increase the digit before ours by one
+        if(this->wholeNumber[position] > 9)
+        {
+            if(position - 1 >= 0) this->wholeNumber[position - 1] ++;
+            else
+            {
+                // if we are in the first index (0) we should append this one in the front
+                this->wholeNumber.push_front(1);
+                this->wholeNumber[1] %= 10;
+                break;
+            }
+            this->wholeNumber[position] %= 10;
+        }
+        position --;
+    }
+}
+
+void BigDecimalInt::doSubtraction(BigDecimalInt& a) {
+    // (this class) should have the smallest wholeNumber
+    if(*this > a) swap(this->wholeNumber, a.wholeNumber);
+    else this->sign = !this->sign;
+    int position = (int)this->wholeNumber.size() - 1;
+    while (position >= 0)
+    {
+        this->wholeNumber[position] = a.wholeNumber[position] - this->wholeNumber[position];
+        if(this->wholeNumber[position] < 0)
+        {
+            if(position)
+            {
+                a.wholeNumber[position - 1] --;
+                this->wholeNumber[position] += 10;
+            }
+        }
+        position --;
+    }
 }
 
 BigDecimalInt BigDecimalInt::operator+(BigDecimalInt a)
 {
     // if the two numbers has the different sign
-    // then we should do subtraction instead of summation of these two number classes
+    // then we should do subtraction instead of summation of these two wholeNumber classes
+    equalNumberLength(*this, a);
     if(this->sign != a.sign)
     {
         a.sign = this->sign;
@@ -44,18 +118,18 @@ BigDecimalInt BigDecimalInt::operator+(BigDecimalInt a)
     }
     BigDecimalInt tmp("");
     tmp = *this;
-    // make the length of the two number classes equal each other
+    // make the length of the two wholeNumber classes equal each other
     // the smallest size will append zeros in front of it
     equalNumberLength(tmp, a);
     tmp.doSummation(a);
-    tmp.deleteZeros();
     return tmp;
 }
 
 BigDecimalInt BigDecimalInt::operator-(BigDecimalInt a)
 {
     // if the two numbers has the different sign
-    // then we should do summation instead of subtraction of these two number classes
+    // then we should do summation instead of subtraction of these two wholeNumber classes
+    equalNumberLength(*this, a);
     if(this->sign != a.sign)
     {
         a.sign = this->sign;
@@ -63,128 +137,104 @@ BigDecimalInt BigDecimalInt::operator-(BigDecimalInt a)
     }
     BigDecimalInt tmp("");
     tmp = *this;
-    // make the length of the two number classes equal each other
+    // make the length of the two wholeNumber classes equal each other
     // the smallest size will append zeros in front of it
     equalNumberLength(tmp, a);
     tmp.doSubtraction(a);
-    tmp.deleteZeros();
+    tmp.deleteZerosFromLeft();
     return tmp;
 }
 
 BigDecimalInt& BigDecimalInt::operator=(const BigDecimalInt& a)
 {
-    this->number = a.number;
+    this->wholeNumber = a.wholeNumber;
     this->sign = a.sign;
+    return *this;
+}
+
+BigDecimalInt &BigDecimalInt::operator=(BigDecimalInt &&anotherNumber) noexcept {
+    this->wholeNumber = anotherNumber.wholeNumber;
+    this->sign = anotherNumber.sign;
     return *this;
 }
 
 void BigDecimalInt::equalNumberLength(BigDecimalInt &a, BigDecimalInt &b)
 {
-    // make the length of the two number classes equal each other
+    // make the length of the two wholeNumber classes equal each other
     // the smallest size will append zeros in front of it
-    int dif = abs(a.number.size() - b.number.size());
-    if(a.number.size() < b.number.size())
+    int dif = abs(a.wholeNumber.size() - b.wholeNumber.size());
+    if(a.wholeNumber.size() < b.wholeNumber.size())
         for (int i = 0; i < dif; ++i)
-            a.number.push_front(0);
+            a.wholeNumber.push_front(0);
     else
         for (int i = 0; i < dif; ++i)
-            b.number.push_front(0);
+            b.wholeNumber.push_front(0);
 }
 
-bool BigDecimalInt::operator<(const BigDecimalInt& a)
+bool BigDecimalInt::isSmallerThan(const BigDecimalInt& anotherNumber) {
+    // check if (this) class is smaller than the anotherNumber class
+    if(this->wholeNumber.size() != anotherNumber.wholeNumber.size())
+        return (this->wholeNumber.size() < anotherNumber.wholeNumber.size());
+    int length = this->wholeNumber.size();
+    for (int i = 0; i < length; ++i) {
+        if(this->wholeNumber[i] == anotherNumber.wholeNumber[i]) continue;
+        return (this->wholeNumber[i] < anotherNumber.wholeNumber[i]);
+    }
+    return false;
+}
+
+bool BigDecimalInt::isGreaterThan(const BigDecimalInt& anotherNumber) {
+    // check if (this) class is greater than the anotherNumber class
+    if(this->wholeNumber.size() != anotherNumber.wholeNumber.size())
+        return (this->wholeNumber.size() > anotherNumber.wholeNumber.size());
+    int length = this->wholeNumber.size();
+    for (int i = 0; i < length; ++i) {
+        if(this->wholeNumber[i] == anotherNumber.wholeNumber[i]) continue;
+        return (this->wholeNumber[i] > anotherNumber.wholeNumber[i]);
+    }
+    return false;
+}
+
+bool BigDecimalInt::operator<(const BigDecimalInt& anotherNumber)
 {
-    if(this->sign != a.sign) return this->sign < a.sign;
-    return this->number < a.number;
-}
-
-bool BigDecimalInt::operator>(const BigDecimalInt& a) {
-    if(this->sign != a.sign) return this->sign > a.sign;
-    return this->number > a.number;
-}
-
-bool BigDecimalInt::operator==(const BigDecimalInt& a) {
-    if(this->sign != a.sign) return 0;
-    return this->number == a.number;
-}
-
-std::ostream &operator<<(std::ostream &output, BigDecimalInt a) {
-    // if the number is 0, then we should print only 0 without any sign
-    if(a.number.size() == 1 && !a.number[0])
-    {
-        output << 0;
-        return output;
+    // compare the two BigDecimalNumbers and return if (this) class smaller than the authorNumber class
+    if(this->sign != anotherNumber.sign)
+        return this->sign < anotherNumber.sign;
+    if(!this->sign){
+        return this->isGreaterThan(anotherNumber);
+    }else{
+        return this->isSmallerThan(anotherNumber);
     }
-    // print first the sign before the number
-    output << (a.sign ? '+' : '-');
-    for(auto val : a.number)
-    {
+}
+
+bool BigDecimalInt::operator>(const BigDecimalInt& anotherNumber) {
+    // compare the two BigDecimalNumbers and return if (this) class smaller than the authorNumber class
+    if(this->sign != anotherNumber.sign)
+        return this->sign > anotherNumber.sign;
+    if(!this->sign){
+        return this->isSmallerThan(anotherNumber);
+    }else{
+        return this->isGreaterThan(anotherNumber);
+    }
+}
+
+bool BigDecimalInt::operator==(const BigDecimalInt& anotherNumber) {
+    if(this->sign != anotherNumber.sign) return false;
+    return this->wholeNumber == anotherNumber.wholeNumber;
+}
+
+std::ostream &operator<<(std::ostream &output, const BigDecimalInt& anotherNumber) {
+    // if the number is negative, then we must print the negative sign first before the number
+    if(!anotherNumber.sign) output << '-';
+    for(auto val : anotherNumber.wholeNumber)
         output << val;
-    }
     return output;
 }
 
-void BigDecimalInt::deleteZeros() {
-    // delete leading zeros from the beginning of the number (from right)
-    while (this->number.size() > 1 && !this->number.front())
-        this->number.pop_front();
-    if(this->number.size() == 1 && !this->number[0]) this->sign = 1;
-}
-
-int BigDecimalInt::isValidNumber(std::string &number) {
-    int start;
-    if(number[0] == '-' || number[0] == '-') start = 1;
-    else start = 0;
-    for (int i = start; i < number.size(); ++i)
-    {
-        if(number[i] < '0' || number[i] > '9')
-        {
-            std::cout << "No valid number!\n";
-            return -1;
-        }
-    }
-    return start;
-}
-
-void BigDecimalInt::doSummation(BigDecimalInt& a)
-{
-    // do summation between digits
-    int position = (int)this->number.size() - 1;
-    while (position >= 0){
-        this->number[position] += a.number[position];
-        // if the number is greater than 9
-        // we should increase the digit before ours by one
-        if(this->number[position] > 9)
-        {
-            if(position - 1 >= 0) this->number[position - 1] ++;
-            else
-            {
-                // if we are in the first index (0) we should append this one in the front
-                this->number.push_front(1);
-                this->number[1] %= 10;
-                break;
-            }
-            this->number[position] %= 10;
-        }
-        position --;
-    }
-}
-
-void BigDecimalInt::doSubtraction(BigDecimalInt& a) {
-    // (this class) should have the smallest number
-    if(this->number > a.number) swap(this->number, a.number);
-    else this->sign = !this->sign;
-    int position = (int)this->number.size() - 1;
-    while (position >= 0)
-    {
-        this->number[position] = a.number[position] - this->number[position];
-        if(this->number[position] < 0)
-        {
-            if(position)
-            {
-                a.number[position - 1] --;
-                this->number[position] += 10;
-            }
-        }
-        position --;
-    }
+std::istream &operator>>(std::istream &input, BigDecimalInt &number) {
+    // get BigDecimalInt from user input
+    std::string realNumberString;
+    std::cin >> realNumberString;
+    number = BigDecimalInt(realNumberString);;
+    return input;
 }
--- Project files/BigDecimalInt.h
+++ Project files/BigDecimalInt.h
@@ -1,29 +1,41 @@
+/*
+ * BigDecimalInt class is a big container of the integer numbers
+ * can do summation or subtraction on these classes
+ * can be created from string, integer number or another BigDecimalInt number
+*/
+
 #ifndef PROJECT_BIGDECIMALINT_H
 #define PROJECT_BIGDECIMALINT_H
 #include <deque>
-#include <ostream>
+#include <iostream>
 #include <regex>
+
 class BigDecimalInt {
-    int size;
     bool sign;
-    std::deque<int> number;
+    std::deque<int> wholeNumber;
     void equalNumberLength(BigDecimalInt& a, BigDecimalInt& b);
-    void deleteZeros();
-    static int isValidNumber(std::string& number);
+    void deleteZerosFromLeft();
+    int isValidNumber(const std::string& number);
     void doSummation(BigDecimalInt& a);
     void doSubtraction(BigDecimalInt& a);
+    void setNumber(const std::string& number);
+    bool isSmallerThan(const BigDecimalInt& secondNumber);
+    bool isGreaterThan(const BigDecimalInt& secondNumber);
 public:
-    explicit BigDecimalInt(int n);
-    explicit BigDecimalInt(std::string n);
-    BigDecimalInt operator+ (BigDecimalInt a);
-    BigDecimalInt operator- (BigDecimalInt a);
-    BigDecimalInt& operator= (const BigDecimalInt& a);
-    bool operator< (const BigDecimalInt& a);
-    bool operator> (const BigDecimalInt& a);
-    bool operator== (const BigDecimalInt& a);
-    friend std::ostream& operator<<(std::ostream& output, BigDecimalInt a);
+    BigDecimalInt()= default;
+    explicit BigDecimalInt(const int& number);
+    explicit BigDecimalInt(const std::string& number);
+    BigDecimalInt(const BigDecimalInt &anotherReal);
+    BigDecimalInt(BigDecimalInt &&anotherNumber) noexcept;
+    BigDecimalInt operator+ (BigDecimalInt anotherNumber);
+    BigDecimalInt operator- (BigDecimalInt anotherNumber);
+    BigDecimalInt& operator= (const BigDecimalInt& anotherNumber);
+    BigDecimalInt& operator= (BigDecimalInt && anotherNumber) noexcept;
+    bool operator< (const BigDecimalInt& anotherNumber);
+    bool operator> (const BigDecimalInt& anotherNumber);
+    bool operator== (const BigDecimalInt& anotherNumber);
+    friend std::ostream& operator<<(std::ostream& output, const BigDecimalInt& number);
+    friend std::istream& operator>>(std::istream& input, BigDecimalInt& number);
 };
 
-
-
-#endif //PROJECT_BIGDECIMALINT_H
+#endif
